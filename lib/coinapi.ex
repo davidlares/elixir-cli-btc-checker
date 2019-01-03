68d560d10d19cb1b75547f92ab0e72b8e6170721
David E Lares S <david.e.lares@gmail.com>
2019-01-03 09:34:41 -0600

    Non tested CoinMarketCap API Integration

--- lib/coinapi.ex
+++ lib/coinapi.ex
@@ -1,17 +1,53 @@
 defmodule Coinapi do
-  @moduledoc """
-  Documentation for Coinapi.
-  """
 
-  @doc """
-  Hello world.
+  # we are using bitcoin, so the method is called like this
+  def bitcoin do
+    # piping requests
+    request("https://api.coinmarketcap.com/v1/ticker/bitcoin")
+    |> body
+    |> parse_body_to_tuple
+    |> get_list_per_tuple
+    |> get_map_to_list
+    |> create_message
+    |> show_message
 
-  ## Examples
+  end
 
-      iex> Coinapi.hello()
-      :world
+  # defining GET request
+  def request(url) do
+    HTTPotion.get url
+  end
 
-  """
+  # getting response body
+  def body(response) do
+    response.body
+  end
+
+  # parsing body to tuple
+  def parse_body_to_tuple(body) do
+    Poison.Parser.parse body
+  end
+
+  # getting the list per Tuple
+  def get_list_per_tuple(tuple) do
+    elem(tuple,1)
+  end
+
+  # getting the map from the list
+  def get_map_to_list(list) do
+    list |> Enum.at 0
+  end
+
+  # creating custom message
+  def create_message(map) do
+    "> #{map["name"]} (#{map["symbol"]}) have an actual price of #{map["price_usd"]} USD"
+  end
+
+  # showing the custom message
+  def show_message(message) do
+    IO.puts message
+  end
+
   def hello do
     :world
   end
